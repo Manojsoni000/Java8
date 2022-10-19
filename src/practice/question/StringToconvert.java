0f15af0fb9ea0a6b82055c55d574e149edf162de
Manoj Soni <manoj.soni@astegic.in>
2022-10-19 14:33:51 +0530

    Update push

--- src/practice/question/StringToconvert.java
+++ src/practice/question/StringToconvert.java
@@ -3,25 +3,30 @@ package practice.question;
 import java.util.Scanner;
 
 public class StringToconvert {
-	public static void main(String[] args) {
-		String str;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Please Enter Any String");
-		str = sc.next();
 
-		int len = str.length();
-
-		System.out.println(str);
-
+	public static String checkString(String str, int len) {
+		int count = 1;
+		char c = 0;
+		String empstring = "";
+		String final_str = "";
 		for (int i = 0; i < len; i++) {
-			int count = 1;
-
 			while (i < (len - 1) && str.charAt(i) == str.charAt(i + 1)) {
+				c = str.charAt(i);
 				count++;
 				i++;
+				empstring = count + "" + c;
 			}
-
-			System.out.print(count + "" + str.charAt(i));
+			final_str = final_str + empstring;
+			count = 1;
 		}
+		return final_str;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please Enter Any String");
+		String str = sc.next();
+		int len = str.length();
+		System.out.println(checkString(str, len));
 	}
 }
